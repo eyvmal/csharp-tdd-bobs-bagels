895c43a7606ddbbed8ea3d92a858701dc2ce256e
Eyvind <eyvind.malde@gmail.com>
2024-08-14 13:57:38 +0200

    add more tests and refine old ones

--- csharp-tdd-bobs-bagels.tests/BobBagelsTest.cs
+++ csharp-tdd-bobs-bagels.tests/BobBagelsTest.cs
@@ -10,12 +10,25 @@ public class BobBagelsTests
     {
     }
 
-    [Test]
-    public void AddBagelTest()
+    [TestCase("b1", 5, true)]
+    [TestCase("b2", 1, false)]
+    public void AddBagelTest(string a, int b, bool c)
+    {
+        Basket basket = new Basket(b);
+
+        basket.addBagel("b0");
+        bool result = basket.addBagel(a);
+        Assert.That(result, Is.EqualTo(c));
+    }
+
+    [TestCase("b1", true)]
+    [TestCase("b2", false)]
+    public void RemoveBagelTest(string a, bool b)
     {
         Basket basket = new Basket(5);
-        bool result = basket.addBagel("b1");
 
-        Assert.That(result, Is.True);
+        basket.addBagel("b1");
+        bool result = basket.removeBagel(a);
+        Assert.That(result, Is.EqualTo(b));
     }
 }
